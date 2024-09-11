7c99c9d88763d68680d292e7ca3d06319f8e90f2
adityabhamare10 <adityabhamare71@gmail.com>
2024-09-11 10:55:33 +0530

    Google Admob -> Banner Ads

--- app/src/main/java/com/hustler/beatwish/MainActivity.java
+++ app/src/main/java/com/hustler/beatwish/MainActivity.java
@@ -10,16 +10,44 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 import com.google.firebase.FirebaseApp;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView artist, attendee;
 
+    AdView adView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Thread(
+                () -> {
+                    // Initialize the Google Mobile Ads SDK on a background thread.
+                    MobileAds.initialize(this, initializationStatus -> {});
+                })
+                .start();
+
+        adView = findViewById(R.id.adView);
+
+        // Create a new ad view.
+//        adView = new AdView(this);
+//        adView.setAdUnitId("ca-app-pub-8048594935916934/5493002503");
+//        adView.setAdSize(getAdSize());
+
+//     // Replace ad container with new ad view.
+//        adContainerView.removeAllViews();
+//        adContainerView.addView(adView);
+
+
+        // Start loading the ad in the background.
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
 
         attendee = findViewById(R.id.attendee);
         artist = findViewById(R.id.artist);
